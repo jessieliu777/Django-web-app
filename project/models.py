6bb8435d9c3d3ff84036676b7659e7e69cdc1f5f
jessieliu777 <34192187+jessieliu777@users.noreply.github.com>
2022-08-31 09:30:09 -0400

    Added Django web app

---
+++ project/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length = 20)
+    def __str__ (self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length = 20)
+    def __str__ (self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length = 20)
+    date_created = models.DateField()
+    # when the category is deleted, also deleted the product
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    tag = models.ManyToManyField(Tag)
+    def __str__ (self):
+        return self.name
+
---
+++ project/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+from django.db.models import Q
+from project.models import Product
+
+# Create your views here.
+def index(request):
+    return render(request, 'project/index.html')
+
+def search_results(request):
+    if (request.method == "POST"):
+        searched = request.POST['searched']
+        products = Product.objects.filter(Q(name__contains=searched) | Q(category__name__contains=searched) | Q(tag__name__contains=searched)).distinct()
+        return render(request, 'project/search_results.html', {'searched': searched, 'products': products})
+    else:
+        return render(request, 'project/search_results.html')
+
+def products_all(request):
+    products_list = Product.objects.all()
+    return render(request, 'project/products_all.html', {'products_list':products_list})
+
+def filter_results(request):
+    if (request.method == "POST"):
+        name = request.POST['name']
+        category = request.POST['category']
+        tag = request.POST['tag']
+        products = Product.objects.filter(Q(name__contains=name) & Q(category__name__contains=category) & Q(tag__name__contains=tag)).distinct()
+        return render(request, 'project/filter_results.html', {'products': products})
+    else:
+        return render(request, 'project/filter_results.html')
