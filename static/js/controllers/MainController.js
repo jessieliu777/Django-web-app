06b2962679448a49f7b5d657d922fda469c348c5
jessieliu777 <34192187+jessieliu777@users.noreply.github.com>
2022-09-27 18:38:04 -0400

    use var instead of scope

--- static/js/controllers/MainController.js
+++ static/js/controllers/MainController.js
@@ -1,89 +1,95 @@
 // scope is something everyone can talk to
-app.controller('MainController', function($scope, $http, $log, MainService) {
+app.controller('MainController', function ($scope, $http, $log, MainService) {
     var va = this;
-    $scope.greeting = "Welcome!";
-    $scope.products = [];
-    $scope.page = 0;
-    $scope.page_size = 10;
-    $scope.get = function() {
-        MainService.get('/api/products/' + '?page=' + $scope.page + '&page_size=' + $scope.page_size)
+    va.greeting = "Welcome!";
+    va.products = [];
+    va.page = 0;
+    va.page_size = 10;
+    va.newProduct = {};
+//    $scope.newCategory = {};
+
+    $scope.errors = {};
+    va.query = {};
+    va.keyword = '';
+
+    va.getProducts = getProducts;
+    va.getCategories = getCategories;
+    va.getTags = getTags;
+    va.postProduct = postProduct;
+    va.searchProducts = searchProducts;
+    va.filterProducts = filterProducts;
+
+    function getProducts() {
+        MainService.get('/api/products/' + '?page=' + va.page + '&page_size=' + va.page_size)
         .then(function(res){
-          $scope.products = res;
+          va.products = res;
         },function(err) {
           console.error('Error while getting data');
         });
     };
 
-    $scope.getCategories = function() {
+    function getCategories() {
         MainService.get('/api/categories/').then(function(res){
           $scope.categories = res;
         });
     };
 
-    $scope.getTags= function() {
+    function getTags() {
         MainService.get('/api/tags/').then(function(res){
-          $scope.tags = res;
+          va.tags = res;
         });
     };
 
-    $scope.get();
-    $scope.getCategories();
-    $scope.getTags();
+    va.getProducts();
+    va.getCategories();
+    va.getTags();
 
-    $scope.newProduct = {};
-    $scope.newCategory = {};
-    $scope.newTags = {};
-
-    $scope.errors = {};
-
-    $scope.post = function() {
-        MainService.post('/api/products/', $scope.newProduct, $scope.errors)
+    function postProduct() {
+        MainService.post('/api/products/', va.newProduct, $scope.errors)
         .then(function(res){
-          $scope.newProduct = {};
+          va.newProduct = {};
         }, function(err) {
             console.error('Error while posting product');
         });
     };
 
-    $scope.postCategory = function() {
-        MainService.post('/api/categories/', $scope.newCategory, $scope.errors).then(function(res){
-          $scope.newCategory = {};
-        }, function(err) {
-            console.error('Error while posting category');
-        });
-    };
+//    $scope.postCategory = function() {
+//        MainService.post('/api/categories/', $scope.newCategory, $scope.errors).then(function(res){
+//          $scope.newCategory = {};
+//        }, function(err) {
+//            console.error('Error while posting category');
+//        });
+//    };
 
-    $scope.query = {};
-    $scope.keyword = '';
 
-    $scope.searchProducts = function(product) {
-        searchName = product.name.toLowerCase().includes($scope.keyword.toLowerCase());
-        searchCategory = product.category.name.toLowerCase().includes($scope.keyword.toLowerCase());
+   function searchProducts(product) {
+        searchName = product.name.toLowerCase().includes(va.keyword.toLowerCase());
+        searchCategory = product.category.name.toLowerCase().includes(va.keyword.toLowerCase());
         searchTag = false;
         for (let i = 0; i < product.tag.length; i++) {
-            searchTag = searchTag || (product.tag[i].name.toLowerCase().includes($scope.keyword.toLowerCase()));
+            searchTag = searchTag || (product.tag[i].name.toLowerCase().includes(va.keyword.toLowerCase()));
             // if none of the product tags contains query tag, then the product doesn't contain it
             if (searchTag) break;
         }
         return searchName || searchCategory || searchTag;
     };
 
-    $scope.filterProducts = function(product) {
+    function filterProducts(product) {
         filterName = true;
         filterCategory = true;
         filterTag = true;
-        if ($scope.query.name){
-            filterName = product.name.toLowerCase().includes($scope.query.name.toLowerCase());
+        if (va.query.name){
+            filterName = product.name.toLowerCase().includes(va.query.name.toLowerCase());
         }
-        if ($scope.query.category){
-            filterCategory = product.category.id === $scope.query.category.id;
+        if (va.query.category){
+            filterCategory = product.category.id === va.query.category.id;
         }
-        if ($scope.query.tag){
-            for (let i = 0; i < $scope.query.tag.length; i++) {
+        if (va.query.tag){
+            for (let i = 0; i < va.query.tag.length; i++) {
                 ithFilter = false;
                 // as long as one product tag contains the query tag, then the product contains the query tag
                 for (let j = 0; j < product.tag.length; j++){
-                    ithFilter = ithFilter || (product.tag[j].id == $scope.query.tag[i].id);
+                    ithFilter = ithFilter || (product.tag[j].id == va.query.tag[i].id);
                     if (ithFilter) break;
                 }
                 filterTag = filterTag && ithFilter;
